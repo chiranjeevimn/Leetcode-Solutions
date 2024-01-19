9df16b3947dafe44c4d8bbe852b16c416e55fa98
Chiranjeevi M N <135362312+chiranjeevimn@users.noreply.github.com>
2024-01-19 22:37:53 +0530

    Time: 122 ms (55.05%), Space: 19.1 MB (75.60%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        
+        for num in nums:
+            result ^= num
+        
+        return result
+        # Time Complexity: O(n)
+        # Space Complexity: O(1)
