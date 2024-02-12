50eec35d24e95ed2b3f9f6409db22a94eaa8012c
Chiranjeevi M N <135362312+chiranjeevimn@users.noreply.github.com>
2024-02-12 21:09:06 +0530

    Time: 2 ms (78.11%), Space: 50.4 MB (54.54%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int candidate = nums[0];
+        int count = 1;
+
+        // Find the majority element candidate
+        for (int i = 1; i < nums.length; i++) {
+            if (count == 0) {
+                candidate = nums[i];
+                count = 1;
+            } else if (nums[i] == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        // Verify if the candidate is the majority element
+        count = 0;
+        for (int num : nums) {
+            if (num == candidate) {
+                count++;
+            }
+        }
+
+        return (count > nums.length / 2) ? candidate : -1;
+    }
+}
