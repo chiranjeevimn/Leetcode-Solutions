7d66eed32f6ce97e51f9568a2bc2fc2f9845bed7
Chiranjeevi M N <135362312+chiranjeevimn@users.noreply.github.com>
2024-02-29 20:49:54 +0530

    Time: 91 ms (88.93%), Space: 45.3 MB (89.44%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cs
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public IList<int> PostorderTraversal(TreeNode root) {
+        List<int> result = new List<int>();
+        PostorderTraversalHelper(root, result);
+        return result;
+    }
+
+    private void PostorderTraversalHelper(TreeNode node, List<int> result) {
+        if (node != null) {
+            PostorderTraversalHelper(node.left, result);
+            PostorderTraversalHelper(node.right, result);
+            result.Add(node.val);
+        }
+    }
+}
